02549e58f260915e4ccdc1a036610580c4f2adee
parrt <parrt@cs.usfca.edu>
2017-08-26 11:07:58 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lolviz',
-    version='1.0',
+    version='1.0.1',
     url='https://github.com/parrt/lolviz',
     license='BSD',
     author='Terence Parr',
