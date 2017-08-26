b6b3849c5176cdce550cfa4f5d83ccef62daa3e6
parrt <parrt@cs.usfca.edu>
2017-08-26 10:25:06 -0700

    fix dependency list

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='lolviz',
     version='1.0',
-    packages=['lolviz']
-    install_requires=['graphviz']
+    packages=['lolviz'],
     url='https://github.com/parrt/lolviz',
     license='BSD',
     author='Terence Parr',
     author_email='parrt@antlr.org',
-    description='A simple Python data-structure visualization tool for lists of lists, lists, dictionaries'
+    description='A simple Python data-structure visualization tool for lists of lists, lists, dictionaries',
+    keywords=['visualization', 'data structures']
 )
