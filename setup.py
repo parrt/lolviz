36073efe1cbc6958ef75f1bdace84d3afe07939c
parrt <parrt@cs.usfca.edu>
2017-08-26 09:58:22 -0700

    initial setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lolviz',
+    version='1.0',
+    packages=['lolviz']
+    url='https://github.com/parrt/lolviz',
+    license='BSD',
+    author='Terence Parr',
+    author_email='parrt@cs.usfca.edu',
+    description='A simple Python data-structure visualization tool for lists of lists, lists, dictionaries'
+)
