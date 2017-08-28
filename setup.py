f676289151091b56d50d98887dc66c09a8d137b5
parrt <parrt@cs.usfca.edu>
2017-08-28 15:58:32 -0700

    Fixes #2. Dont use -> For tuples that are values of dictionary key-value pairs. bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lolviz',
-    version='1.1',
+    version='1.1.1',
     url='https://github.com/parrt/lolviz',
     license='BSD',
     py_modules=['lolviz'],
