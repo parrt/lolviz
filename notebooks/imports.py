f0503c388344462f6ae776278016ab40df6d2319
groverpr <pgrover3@dons.usfca.edu>
2018-08-13 10:38:54 -0700

    visualize sklearn decision tree structure using graphviz

--- notebooks/imports.py
+++
@@ -1,8 +0,0 @@
-import IPython, graphviz, re
-import pydotplus
-from io import StringIO
-from IPython.display import Image  
-import numpy as np
-import pandas as pd
-from sklearn import tree
-from sklearn.datasets import load_boston, load_iris
