5b542d2163fc3896fe478a60609d4017c97d8d8e
parrt <parrt@cs.usfca.edu>
2019-08-05 11:54:35 -0700

    add new examples

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lolviz',
-    version='1.4.2',
+    version='1.4.3',
     url='https://github.com/parrt/lolviz',
     license='BSD',
     py_modules=['lolviz'],
